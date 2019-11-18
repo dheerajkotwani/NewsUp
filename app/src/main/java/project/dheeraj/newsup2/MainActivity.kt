6e0bc09f7799d6dce98ca90859fd5857bc33b79f
dheerajkotwani <dheeraj.kotwani41@gmail.com>
2019-11-18 22:48:42 +0530

    Add layouts

---
+++ app/src/main/java/project/dheeraj/newsup2/MainActivity.kt
@@ -0,0 +1,12 @@
+package project.dheeraj.newsup2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/project/dheeraj/newsup2/myNews.kt
@@ -0,0 +1,6 @@
+package project.dheeraj.newsup2
+
+data class myNews(
+    var headline : String = "",
+    var content : String = ""
+)
