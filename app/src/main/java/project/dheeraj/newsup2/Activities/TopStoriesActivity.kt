2a3734b999a5d55982a683c64b7d91da762ab6d2
dheerajkotwani <dheeraj.kotwani41@gmail.com>
2019-11-22 02:31:00 +0530

    Attach activity for top headlines

---
+++ app/src/main/java/project/dheeraj/newsup2/Activities/TopStoriesActivity.kt
@@ -0,0 +1,13 @@
+package project.dheeraj.newsup2.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.dheeraj.newsup2.R
+
+class top_stories_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_stories_activity)
+    }
+}
