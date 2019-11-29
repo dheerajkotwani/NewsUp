a1afb142cf9cdf30c2144435ba338cc593f4a7e6
dheerajkotwani <dheeraj.kotwani41@gmail.com>
2019-11-29 12:35:43 +0530

    Add developer page

---
+++ app/src/main/java/project/dheeraj/newsup2/Activities/DeveloperInfoActivity.kt
@@ -0,0 +1,12 @@
+package project.dheeraj.newsup2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DeveloperInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_developer_info)
+    }
+}
